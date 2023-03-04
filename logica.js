2b2ea8cefa6ee13a8741b14698c2181112931ebb
jegarcia2 <jegarcia2@tes.edu.ec>
2023-03-04 13:15:54 -0500

    Inicio del repositorio con base de la guía Alura

---
+++ logica.js
@@ -0,0 +1,12 @@
+function btnEncriptar(textAreaEntrada,textAreaSalida){
+    var texto = textAreaEntrada.value
+    textAreaSalida.value ="hello"
+    
+
+}
+
+function btnDesencriptar(textAreaEntrada,textAreaSalida){
+    var texto = textArea.value
+    var palabras = texto.split(" ")
+    
+}
