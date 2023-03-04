8b78dc20ca53e57a4ddcc89c45af455db2b77988
jegarcia2 <jegarcia2@tes.edu.ec>
2023-03-04 16:49:20 -0500

    Logica funcional

--- logica.js
+++ logica.js
@@ -1,12 +1,33 @@
 function btnEncriptar(textAreaEntrada,textAreaSalida){
-    var texto = textAreaEntrada.value
-    textAreaSalida.value ="hello"
-    
-
+    var texto = (textAreaEntrada.value).toLowerCase()
+    var map = {
+        "a": "ai",
+        "e": "enter",
+        "i": "imes",
+        "o": "ober",
+        "u": "ufat"
+            }
+    texto = texto.replace(/a|e|i|o|u/gi, function(matched){
+        return map[matched];
+    })
+    textAreaSalida.value =texto
 }
 
 function btnDesencriptar(textAreaEntrada,textAreaSalida){
-    var texto = textArea.value
-    var palabras = texto.split(" ")
+    var texto = (textAreaEntrada.value).toLowerCase()
+    var map = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u"
+            }
+    texto = texto.replace(/ai|enter|imes|ober|ufat/gi, function(matched){
+        return map[matched];
+    })
+    textAreaSalida.value =texto
+}
+
+function btnCopiar(){
     
 }
